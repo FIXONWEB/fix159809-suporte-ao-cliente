326ff3805ad563c01fdab5c162d19a49911596e3
fixonweb <contato@fixonweb.com.br>
2020-08-22 09:30:20 -0300

    0.1.3

--- add-in/1598096194-mapas.php
+++ add-in/1598096194-mapas.php
@@ -10,8 +10,14 @@ function fix1598096194_mapa1($atts, $content = null){
 	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
  	<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
 
-	<script type="text/javascript">
 
+	<style type="text/css" media="screen">
+		#mapid { height: 180px; }
+	</style>
+	<div id="mapid"></div>
+
+	<script type="text/javascript">
+		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 	</script>
 
 	<?php
