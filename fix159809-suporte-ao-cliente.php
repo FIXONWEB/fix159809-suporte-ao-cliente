967c79086f2b738f6b43e0e1bc7f70da6caebff0
fixonweb <contato@fixonweb.com.br>
2020-08-22 09:27:04 -0300

    0.1.2

--- fix159809-suporte-ao-cliente.php
+++ fix159809-suporte-ao-cliente.php
@@ -7,7 +7,7 @@
  * Author URI:      https://fixonweb.com.br
  * Text Domain:     fix159809-suporte-ao-cliente
  * Domain Path:     /languages
- * Version:         0.1.1
+ * Version:         0.1.2
  *
  * @package         Fix159809_Suporte_Ao_Cliente
  */
